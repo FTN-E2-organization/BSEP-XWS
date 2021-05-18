88c0c0001078c7b5660a591c99b8c78b5255b1d1
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-05-18 13:48:57 +0200

    init:user-service (micronaut)

---
+++ Nistagram/user-service/src/main/java/app/controller/Hello.java
@@ -0,0 +1,14 @@
+package app.controller;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello") 
+public class Hello {
+
+	@Get(produces = MediaType.TEXT_PLAIN) 
+    public String index() {
+        return "Hello World"; 
+    }
+}
