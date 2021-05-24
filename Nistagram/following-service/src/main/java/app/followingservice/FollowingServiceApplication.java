eccb0642dfe27e194b72d66b4ae447f52c57b0b6
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-05-24 14:12:55 +0200

    feat: init following service

---
+++ Nistagram/following-service/src/main/java/app/followingservice/FollowingServiceApplication.java
@@ -0,0 +1,13 @@
+package app.followingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowingServiceApplication.class, args);
+	}
+
+}
---
+++ Nistagram/following-service/src/test/java/app/followingservice/FollowingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.followingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
