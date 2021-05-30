e7e9d2f71719a8293d7df8f44c339e619e82826f
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-05-30 22:05:29 +0200

    chore:add packing to pom.xml

--- Nistagram/auth-service/src/main/java/app/authservice/AuthServiceApplication.java
+++ Nistagram/auth-service/src/main/java/app/authservice/AuthServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
