8cf29cbc70afb06c1aa720bd19ccae2ec58c935d
jelenabudisa <58332793+jelenabudisa@users.noreply.github.com>
2021-05-31 23:56:46 +0200

    feat-create-profile-controller

--- Nistagram/story-service/src/main/java/app/storyservice/controller/ProfileController.java
+++ Nistagram/story-service/src/main/java/app/storyservice/controller/ProfileController.java
@@ -7,14 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.storyservice.dto.ProfileDTO;
 import app.storyservice.model.Profile;
-import app.storyservice.model.Story;
 import app.storyservice.service.ProfileService;
-import app.storyservice.service.StoryService;
 
 @RestController
 @RequestMapping(value = "api/profile")
@@ -42,5 +42,14 @@ public class ProfileController {
 		}
 	} 
 	
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody ProfileDTO profileDTO){
+		try {
+			profileService.create(profileDTO);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		}catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
 }
 
