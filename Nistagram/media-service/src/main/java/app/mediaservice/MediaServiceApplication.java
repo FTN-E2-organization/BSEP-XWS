89a1631888a7716c719c1e6ada08f02f62954cc6
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-06-02 23:12:15 +0200

    feat: register media service with eureka

--- Nistagram/media-service/src/main/java/app/mediaservice/MediaServiceApplication.java
+++ Nistagram/media-service/src/main/java/app/mediaservice/MediaServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.Lifecycle;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ import app.mediaservice.repository.MediaRepository;
 import app.mediaservice.service.MediaService;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MediaServiceApplication implements CommandLineRunner{
 	
 	@Resource
