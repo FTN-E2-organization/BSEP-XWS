3895a44c2e96d0475e9e4fcff3886a1a4d91881f
jelenazeljko55 <jelena_zeljko@uns.ac.rs>
2021-06-02 23:44:25 +0200

    feat: adding reaction (activity service)

--- Nistagram/activity-service/src/main/java/app/activityservice/repository/ReactionRepository.java
+++ Nistagram/activity-service/src/main/java/app/activityservice/repository/ReactionRepository.java
@@ -1,12 +1,13 @@
 package app.activityservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.activityservice.model.Reaction;
 
 public interface ReactionRepository extends JpaRepository<Reaction, Long>{
 
-//	Reaction findByUsernameAndPostID(String ownerUsername, long postId);
-
+	@Query(value = "select * from reaction r, profile p where p.username = ?1 and r.post_id = ?2 and p.id = r.owner_id limit 1", nativeQuery = true)
+	Reaction findByUsernameAndPostID(String ownerUsername, long postId);
 	
 }
--- Nistagram/activity-service/src/main/java/app/activityservice/service/ReactionServiceImpl.java
+++ Nistagram/activity-service/src/main/java/app/activityservice/service/ReactionServiceImpl.java
@@ -24,27 +24,25 @@ public class ReactionServiceImpl implements ReactionService {
 
 	@Override
 	public void create(AddReactionDTO reactionDTO) {
-//		Reaction reaction = reactionRepository.findByUsernameAndPostID(reactionDTO.ownerUsername, reactionDTO.postId);
+		Reaction reaction = reactionRepository.findByUsernameAndPostID(reactionDTO.ownerUsername, reactionDTO.postId);
 		
-//		if (reaction != null) {
-//			if (reaction.getReactionType() == ReactionType.like && reactionDTO.reactionType.equals("like")) {
-//				reactionRepository.delete(reaction);
-//			}
-//			else if (reaction.getReactionType() == ReactionType.like && reactionDTO.reactionType.equals("dislike")) {
-//				reaction.setReactionType(ReactionType.dislike);
-//				reactionRepository.save(reaction);
-//			}
-//			else if (reaction.getReactionType() == ReactionType.dislike && reactionDTO.reactionType.equals("dislike")) {
-//				reactionRepository.delete(reaction);
-//			}
-//			else if (reaction.getReactionType() == ReactionType.dislike && reactionDTO.reactionType.equals("like")) {
-//				reaction.setReactionType(ReactionType.like);
-//				reactionRepository.save(reaction);
-//			}			
-//			return;
-//		}
+		if (reaction != null) {
+			if (reaction.getReactionType() == ReactionType.like && reactionDTO.reactionType.equals("like")) {
+				reaction.setReactionType(ReactionType.no_reaction);
+				reactionRepository.save(reaction);
+			}
+			else if (reaction.getReactionType() == ReactionType.dislike && reactionDTO.reactionType.equals("dislike")) {
+				reaction.setReactionType(ReactionType.no_reaction);
+				reactionRepository.save(reaction);
+			}
+			else {
+				reaction.setReactionType(ReactionType.valueOf(reactionDTO.reactionType));
+				reactionRepository.save(reaction);
+			}			
+			return;
+		}
 				
-		Reaction reaction = new Reaction();
+		reaction = new Reaction();
 		reaction.setReactionType(ReactionType.valueOf(reactionDTO.reactionType));
 		reaction.setPostId(reactionDTO.postId);
 		reaction.setPostType(PostType.valueOf(reactionDTO.postType));
