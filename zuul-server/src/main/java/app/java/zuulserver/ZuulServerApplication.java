49ad156a89a6c32c869837074fa0c97e725479f3
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-06-02 23:29:39 +0200

    refactor: edit port in activity service and add anotation

--- zuul-server/src/main/java/app/java/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/app/java/zuulserver/ZuulServerApplication.java
@@ -2,10 +2,12 @@ package app.java.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
