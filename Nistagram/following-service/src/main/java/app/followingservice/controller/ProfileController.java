8c6db2cbc0f65e848ae62966df747e6881a3c04c
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-06-03 12:51:41 +0200

    fix:config files changed

--- Nistagram/following-service/src/main/java/app/followingservice/controller/ProfileController.java
+++ Nistagram/following-service/src/main/java/app/followingservice/controller/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -149,7 +150,7 @@ public class ProfileController {
 		}
 	}
 	
-	@RequestMapping(path = "/create", method = RequestMethod.POST, consumes = "application/json")
+	@PostMapping(consumes = "application/json")
 	public ResponseEntity<?> addProfile(@RequestBody ProfileDTO userDTO){
 		
 		try {
