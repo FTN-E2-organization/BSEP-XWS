83ae592bfe69be37a02776f4efc54d6ea24a6caa
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-06-04 11:42:41 +0200

    refactor:registration page

--- zuul-server/src/main/resources/static/js/registration.js
+++ zuul-server/src/main/resources/static/js/registration.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
 		};
 		
 		$.ajax({
-			url: "http://localhost:8111/api/auth/profile",
+			url: "/api/auth/profile",
 			type: 'POST',
 			contentType: 'application/json',
 			data: JSON.stringify(profileDTO),
@@ -48,8 +48,8 @@ $(document).ready(function () {
 				$('#div_alert').append(alert);
 				return;
 			},
-			error: function (jqXHR) {
-				let alert = $('<div class="alert alert-danger alert-dismissible fade show m-1" role="alert">ERROR!' +
+			error: function (xhr) {
+				let alert = $('<div class="alert alert-danger alert-dismissible fade show m-1" role="alert">ERROR!' + xhr.responseText + 
 					 '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '</div >')
 				$('#div_alert').append(alert);
 				return;
