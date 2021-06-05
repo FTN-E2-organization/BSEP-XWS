636ef9f70bc37f7a38d47aa5b68493a99fee807a
jelenazeljko55 <jelena_zeljko@uns.ac.rs>
2021-06-06 00:06:58 +0200

    fix: search.js

--- zuul-server/src/main/resources/static/js/search.js
+++ zuul-server/src/main/resources/static/js/search.js
@@ -36,25 +36,29 @@ function getProfilesAndLocationsAndHashtags() {
 
 function getSearchedProfilesAndLocationsAndHashtags() {
 	$('#body_table').empty();
-	
+		
 	let name = $('#name').val();
 	if (name == "")
 		return;
 	
 	for (let i = 0; i < resultsList.length; i++) {
-		if (document.getElementById("all").checked && resultsList[i].contentName.includes(name))
+		if (document.getElementById("all").checked && resultsList[i].contentName.includes(name)) {
 			if (resultsList[i].section == "profile") 
 				addProfileRow(resultsList[i]);
 			else if (resultsList[i].section == "location")
 				addLocationRow(resultsList[i]);		
 			else if (resultsList[i].section == "hashtag")
 				addHashtagRow(resultsList[i]);	
-		else if (resultsList[i].section == "location" && document.getElementById("locations").checked && resultsList[i].contentName.includes(name))
+		}				
+		else if (resultsList[i].section == "location" && document.getElementById("locations").checked && resultsList[i].contentName.includes(name)) {
 			addLocationRow(resultsList[i]);
-		else if (resultsList[i].section == "profile" && document.getElementById("profiles").checked && resultsList[i].contentName.includes(name))
+		}			
+		else if (resultsList[i].section == "profile" && document.getElementById("profiles").checked && resultsList[i].contentName.includes(name)) {
 			addProfileRow(resultsList[i]);
-		else if (resultsList[i].section == "hashtag" && document.getElementById("hashtags").checked && resultsList[i].contentName.includes(name))
+		}
+		else if (resultsList[i].section == "hashtag" && document.getElementById("hashtags").checked && resultsList[i].contentName.includes(name)) {
 			addHashtagRow(resultsList[i]);
+		}
 	}	
 }
 
