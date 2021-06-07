12a5a1b3406c9a9ed5b933f8ff7ea170ef0b410c
jelenazeljko55 <jelena_zeljko@uns.ac.rs>
2021-06-07 23:53:58 +0200

    refactor: sql files updated

--- Nistagram/auth-service/src/main/resources/data-postgres.sql
+++ Nistagram/auth-service/src/main/resources/data-postgres.sql
@@ -4,10 +4,9 @@ INSERT INTO role (id,name) VALUES (1,'ROLE_ADMIN') on conflict (id) do nothing;
 INSERT INTO role (id,name) VALUES (2,'ROLE_REGULAR') on conflict (id) do nothing;
 INSERT INTO role (id,name) VALUES (3,'ROLE_AGENT') on conflict (id) do nothing;
 
-
+/*
 INSERT INTO admin (id, username, email,  password) VALUES 
 			(nextval('users_seq'),'admin', 'admin@mail.com','$2y$12$esSysQuIMJjPmQAxDLNtfuGrTuVn1xBO1ieY.tqMSo46SdUQyMUxC') on conflict (username) do nothing;
-
 INSERT INTO profile (id, username, email, password, name, date_of_birth, gender, is_public) VALUES 
 			(nextval('users_seq'), 'pero123', 'pero123@gmail.com', '$2y$12$esSysQuIMJjPmQAxDLNtfuGrTuVn1xBO1ieY.tqMSo46SdUQyMUxC', 
 			'Pero Peric', '1988-02-25', 0, true) on conflict (username) do nothing;
@@ -18,4 +17,4 @@ INSERT INTO profile (id, username, email, password, name, date_of_birth, gender,
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 1)  on conflict (user_id, role_id) do nothing;
 INSERT INTO users_roles (user_id, role_id) VALUES (2, 2) on conflict (user_id, role_id) do nothing;
 INSERT INTO users_roles (user_id, role_id) VALUES (2, 3) on conflict (user_id, role_id) do nothing;
-INSERT INTO users_roles (user_id, role_id) VALUES (3, 2) on conflict (user_id, role_id) do nothing;
+INSERT INTO users_roles (user_id, role_id) VALUES (3, 2) on conflict (user_id, role_id) do nothing;*/
