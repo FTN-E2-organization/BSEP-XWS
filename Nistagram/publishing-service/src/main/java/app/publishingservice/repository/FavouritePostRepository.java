abb9b6d9255dc7c3798f8e529bdb4bd89fbcfbca
jelenazeljko55 <jelena_zeljko@uns.ac.rs>
2021-06-07 04:07:27 +0200

    fix: FavouritePostServiceImpl

--- Nistagram/publishing-service/src/main/java/app/publishingservice/repository/FavouritePostRepository.java
+++ Nistagram/publishing-service/src/main/java/app/publishingservice/repository/FavouritePostRepository.java
@@ -19,4 +19,9 @@ public interface FavouritePostRepository extends JpaRepository<FavouritePost, Lo
 			" and profile.username=?1 and fp.collection_id=c.id and fp.post_id=p.id ", nativeQuery = true)		
 	Collection<FavouritePost> getAllByUsernameAndCollection(String username, String collectionName);
 
+	@Query(value = "select * from favourite_post fp, profile, post p, collection c " +
+			" where profile.id=fp.owner_id and p.is_deleted=false and c.name=?1 and c.id=fp.collection_id " +
+			" and profile.username=?3 and fp.post_id=p.id and p.id=?2 limit 1", nativeQuery = true)		
+	FavouritePost getByCollectionNameAndPostId(String collectionName, long postId, String ownerUsername);
+
 }
