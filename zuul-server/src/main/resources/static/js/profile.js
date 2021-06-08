596a8b6cb3192b6b4df522e6f7c71173c1ccf12c
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-06-08 13:10:02 +0200

    refactor: edit frontend

--- zuul-server/src/main/resources/static/js/profile.js
+++ zuul-server/src/main/resources/static/js/profile.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
 					success:function(isClose){
 					let close;
 					if(isClose == true){
-						close='<h6 style="color:green;">CLOSE FRIEND</h6><button class="btn btn-info btn-sm" type="button" id="remove_btn" onclick="removeClosed()">REMOVE FROM CLOSES</button>'
+						close='<button class="btn btn-info btn-sm" type="button" id="remove_btn" onclick="removeClosed()">REMOVE FROM CLOSES</button><br><h6 style="color:green;">CLOSE FRIEND</h6>'
 					}else{
 						close='<button class="btn btn-success btn-sm" type="button" id="close_btn" onclick="addClosed()">ADD TO CLOSES</button>'
 					}		
