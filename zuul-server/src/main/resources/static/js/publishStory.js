89d3e2a93836115de8b570f232dc01801947351d
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-06-08 14:38:56 +0200

    refactor:get location at front

--- zuul-server/src/main/resources/static/js/publishStory.js
+++ zuul-server/src/main/resources/static/js/publishStory.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 	
 	/*Click on location*/
 	$("#tableLocations").delegate("tr.location", "click", function(){
-        let location = $('tr.location').text();
+        let location = $(this).text();
         $('#btn_close_location').click();
         $('#location').val(location);
     });
