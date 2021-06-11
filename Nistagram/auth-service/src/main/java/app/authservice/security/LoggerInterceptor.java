d51c6db5e48099c09b03bee3e68b0b635becd000
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-06-11 21:04:10 +0200

    refactor:delete comments and method

--- Nistagram/auth-service/src/main/java/app/authservice/security/LoggerInterceptor.java
+++ Nistagram/auth-service/src/main/java/app/authservice/security/LoggerInterceptor.java
@@ -21,20 +21,12 @@ public class LoggerInterceptor implements HandlerInterceptor {
 			throws Exception {
 		if (request.getRequestURI().equals("/error"))
 			return true;
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		/*String currentPrincipalName = authentication == null ? null : authentication.getName();
-		if (currentPrincipalName == null || currentPrincipalName.equals("anonymousUser"))
-			currentPrincipalName = "";
-		else
-			currentPrincipalName = "User: " + currentPrincipalName;*/
 		String ipAddress = request.getHeader("X-FORWARDED-FOR");
 		if (ipAddress == null) {
 			ipAddress = request.getRemoteAddr();
 		}
-		String body = getParameters(request);
 		try {
-			log.info("Auth service - " + request.getMethod() + " " + request.getRequestURI() + " " + ipAddress + " " 
-					+ " parameters " + body);
+			log.info("Auth service - " + request.getMethod() + " " + request.getRequestURI() + " " + ipAddress);
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
@@ -46,12 +38,6 @@ public class LoggerInterceptor implements HandlerInterceptor {
 			ModelAndView modelAndView) throws Exception {
 		if (request.getRequestURI().equals("/error"))
 			return;
-		/*Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String currentPrincipalName = authentication == null ? null : authentication.getName();
-		if (currentPrincipalName == null || currentPrincipalName.equals("anonymousUser"))
-			currentPrincipalName = "";
-		else
-			currentPrincipalName = "User: " + currentPrincipalName; */
 		String ipAddress = request.getHeader("X-FORWARDED-FOR");
 		if (ipAddress == null) {
 			ipAddress = request.getRemoteAddr();
@@ -62,29 +48,6 @@ public class LoggerInterceptor implements HandlerInterceptor {
 		}catch(Exception ex) {
 			
 		}
-	}
-
-	private String getParameters(HttpServletRequest request) {
-		StringBuffer posted = new StringBuffer();
-		Enumeration<?> e = request.getParameterNames();
-		if (e != null && e.hasMoreElements()) {
-			posted.append("?");
-		}
-
-		while (e.hasMoreElements()) {
-			if (posted.length() > 1) {
-				posted.append("&");
-			}
-			String curr = (String) e.nextElement();
-			posted.append(curr + "=");
-			if (curr.contains("password") || curr.contains("pass") || curr.contains("pwd") ||
-					curr.contains("email")	) {
-				posted.append("*****");
-			} else {
-				posted.append(request.getParameter(curr));
-			}
-		}
-		return posted.toString();
 	}
 
 }
