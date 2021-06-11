8eb23fb7b40b1470de3cda8fe6f996d3ca1c8550
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-06-11 19:49:27 +0200

    refactor:delete comments fron sql script in auth-service

--- Nistagram/auth-service/src/main/resources/data-postgres.sql
+++ Nistagram/auth-service/src/main/resources/data-postgres.sql
@@ -4,8 +4,6 @@ INSERT INTO authority (id,name) VALUES (1,'ROLE_ADMIN');
 INSERT INTO authority (id,name) VALUES (2,'ROLE_REGULAR');
 INSERT INTO authority (id,name) VALUES (3,'ROLE_AGENT');
 
-
-/*INSERT INTO permission (id,name) VALUES (1,'');*/
 INSERT INTO permission (id,name) VALUES (2,'updateProfile');
 INSERT INTO permission (id,name) VALUES (3,'improveProfileAsAgent');
 INSERT INTO permission (id,name) VALUES (4,'findAllowTaggingProfile');
@@ -29,8 +27,6 @@ INSERT INTO permission (id,name) VALUES (21,'getRequests');
 INSERT INTO permission (id,name) VALUES (22,'uploadFile');
 INSERT INTO permission (id,name) VALUES (23,'deleteFile');
 
-
-/*INSERT INTO authorities_permissions (authority_id,permission_id) VALUES (2,1);*/
 INSERT INTO authorities_permissions (authority_id,permission_id) VALUES (2,2);
 INSERT INTO authorities_permissions (authority_id,permission_id) VALUES (1,3);
 INSERT INTO authorities_permissions (authority_id,permission_id) VALUES (2,4);
@@ -55,17 +51,5 @@ INSERT INTO authorities_permissions (authority_id,permission_id) VALUES (2,22);
 INSERT INTO authorities_permissions (authority_id,permission_id) VALUES (2,23);
 
 
-/*
 INSERT INTO admin (id, username, email,  password) VALUES 
 			(nextval('users_seq'),'admin', 'admin@mail.com','$2a$10$aIgiFYlRyaeMbquRwjAlFOvarhu9zNkDRy6jAMmEB6Ry7Njvuwore') on conflict (username) do nothing;
-INSERT INTO profile (id, username, email, password, name, date_of_birth, gender, is_public) VALUES 
-			(nextval('users_seq'), 'pero123', 'pero123@gmail.com', '$2a$10$aIgiFYlRyaeMbquRwjAlFOvarhu9zNkDRy6jAMmEB6Ry7Njvuwore', 
-			'Pero Peric', '1988-02-25', 0, true) on conflict (username) do nothing;
-INSERT INTO profile (id, username, email, password, name, date_of_birth, gender, is_public) VALUES 
-			(nextval('users_seq'), 'ana00', 'ana00@gmail.com', '$2a$10$aIgiFYlRyaeMbquRwjAlFOvarhu9zNkDRy6jAMmEB6Ry7Njvuwore', 
-			'Ana Anic', '2000-12-01', 1, true) on conflict (username) do nothing;
-			
-INSERT INTO users_authorities (user_id, authority_id) VALUES (1, 1) on conflict (user_id, authority_id) do nothing;
-INSERT INTO users_authorities (user_id, authority_id) VALUES (2, 2) on conflict (user_id, authority_id) do nothing;
-INSERT INTO users_authorities (user_id, authority_id) VALUES (2, 3) on conflict (user_id, authority_id) do nothing;
-INSERT INTO users_authorities (user_id, authority_id) VALUES (3, 2)on conflict (user_id, authority_id) do nothing;*/
