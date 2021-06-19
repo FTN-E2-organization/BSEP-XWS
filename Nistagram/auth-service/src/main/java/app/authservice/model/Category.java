f8ae26dd86694a035207dae5b3c41a640e9be59c
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-06-19 20:12:09 +0200

    feat:add categories in profile types in script

--- Nistagram/auth-service/src/main/java/app/authservice/model/Category.java
+++ Nistagram/auth-service/src/main/java/app/authservice/model/Category.java
@@ -1,9 +1,6 @@
 package app.authservice.model;
 
-import java.time.LocalDate;
 import java.util.Set;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,9 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
-
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
