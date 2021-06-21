635330de1883dc17e5d5dba4b4006af22c39243c
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-06-21 16:42:12 +0200

    refactor: data.sql file

--- agent-app/src/main/resources/static/js/navbar/agent.js
+++ agent-app/src/main/resources/static/js/navbar/agent.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('body').prepend($(
         '<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color:white;">'
-        + '<h3 style="margin-right:50px;color:black;"><a class="nav-link .text-dark" href="unauthenticated_index.html" style="color:black;"><b>AGENT APP</b></a></h3>'
+        + '<h3 style="margin-right:50px;color:black;"><a class="nav-link .text-dark" style="color:black;"><b>AGENT APP</b></a></h3>'
         + '<div class="collapse navbar-collapse" id="navbarNav">'
         + ' <ul class="navbar-nav" >'
         + '  <li class="nav-item">'
