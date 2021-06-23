4bbb9888e05100e7f185974f7e1b704d5f658a8e
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-06-23 07:08:54 +0200

    feat: connect campaign service with eureka and zuul

--- Nistagram/campaign-service/src/main/java/app/campaignservice/CampaignServiceApplication.java
+++ Nistagram/campaign-service/src/main/java/app/campaignservice/CampaignServiceApplication.java
@@ -2,8 +2,10 @@ package app.campaignservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CampaignServiceApplication {
 
 	public static void main(String[] args) {
