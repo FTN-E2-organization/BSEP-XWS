bf87e50997e715e1ca273114b033df17031ab404
jelenazeljko55 <jelena_zeljko@uns.ac.rs>
2021-07-02 04:36:09 +0200

    gui: delete campaign (html, js)

--- zuul-server/src/main/resources/static/js/campaignsSetting.js
+++ zuul-server/src/main/resources/static/js/campaignsSetting.js
@@ -6,6 +6,12 @@ var username = "pera";
 
 $(document).ready(function () {
 	
+	getAllCampaigns();
+
+});
+
+
+function getAllCampaigns() {	
 	 $.ajax({
         type: "GET",
         url: "/api/campaign/" + username,
@@ -20,8 +26,8 @@ $(document).ready(function () {
         error: function() {
             console.log('error getting campaigns');
         }
-    }); 
-});
+    }); 	
+}
 
 
 
@@ -31,6 +37,16 @@ function addRow(campaign) {
 		times += ", " + campaign.dailyFrequency[i];
 	}
 	
+	let btnEdit = '<button onclick="fillInTheCheckbox()" class="btn btn-info btn-sm" data-toggle="modal" data-target="#centralModalNotificationSettings" class="btn btn-link" id="btnEdit" >edit</button>';
+	let btnDelete = '<button onclick="deleteCampaign(this.id)" class="btn btn-danger btn-sm" id="' + campaign.id + '" >delete</button>';
+	
+	if (campaign.isDeleted == true || campaign.campaignType == "once_time" /* ili je startDate prosao */) {
+		btnEdit = "";
+	}
+	if (campaign.isDeleted == true) {
+		btnDelete = "";
+	}	
+	
 	let row = $('<tr><td>' + campaign.id + '</td>'
 				+ '<td>' + campaign.campaignType + '</td>'
 				+ '<td>' + campaign.categoryName + '</td>'
@@ -38,8 +54,29 @@ function addRow(campaign) {
 				+ '<td>' + campaign.startDate + '</td>'
 				+ '<td>' + campaign.endDate + '</td>'
 				+ '<td>' + times + '</td>'
-				+ '<td>' + campaign.isDeleted + '</td>'			
-				+ '  </td></tr>');
+				+ '<td>' + campaign.isDeleted + '</td>'	
+				+ '<td>' + btnEdit + '</td>'
+				+ '<td>' + btnDelete + '</td>'			
+				+ ' </tr>');
 				
 	$('#body_table').append(row);			
 }
+
+
+
+function deleteCampaign(campaignId) {
+	 $.ajax({
+        type: "PUT",
+        url: "/api/campaign/" + campaignId,
+        contentType: "application/json",
+        success: function() {
+			console.log("successful deletion")
+			getAllCampaigns();
+        },
+        error: function() {
+            console.log('error deleting campaign');
+        }
+    });	
+}
+
+
