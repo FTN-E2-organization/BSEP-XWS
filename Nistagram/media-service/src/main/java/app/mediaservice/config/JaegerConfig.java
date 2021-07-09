74a9581feecece7ef88d3ada4dcfadd06e516026
jelenabudisa <58332793+jelenabudisa@users.noreply.github.com>
2021-07-09 18:21:40 +0200

    fix-jaeger-docker-and-local-setup

--- Nistagram/media-service/src/main/java/app/mediaservice/config/JaegerConfig.java
+++ Nistagram/media-service/src/main/java/app/mediaservice/config/JaegerConfig.java
@@ -13,19 +13,21 @@ public class JaegerConfig {
 	
 	@Bean
 	public Tracer getTracer() {
-		
+		String host=System.getenv("JAEGER_HOST")==null?"localhost":System.getenv("JAEGER_HOST");
+		String name=System.getenv("JAEGER_SERVICE_NAME")==null?"media-service":System.getenv("JAEGER_SERVICE_NAME");
+	
 		io.jaegertracing.Configuration.SamplerConfiguration samplerConfig = io.jaegertracing.Configuration.SamplerConfiguration
 				.fromEnv().withType(ProbabilisticSampler.TYPE).withParam(1);
 
 		/* Update default sender configuration with custom host and port */
 		SenderConfiguration senderConfig = io.jaegertracing.Configuration.SenderConfiguration.fromEnv()
-				.withAgentHost(System.getenv("JAEGER_HOST")).withAgentPort(6831);
+				.withAgentHost(host).withAgentPort(6831);
 		/* End */
 
 		io.jaegertracing.Configuration.ReporterConfiguration reporterConfig = io.jaegertracing.Configuration.ReporterConfiguration
 				.fromEnv().withLogSpans(true).withSender(senderConfig);
 
-		io.jaegertracing.Configuration config = new io.jaegertracing.Configuration(System.getenv("JAEGER_SERVICE_NAME"))
+		io.jaegertracing.Configuration config = new io.jaegertracing.Configuration(name)
 				.withSampler(samplerConfig).withReporter(reporterConfig);
 
 		return config.getTracer();
